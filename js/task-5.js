0671aec485ea0c98f1a721678f18af167dc43ed9
Alexander Khmyzenko <alexander193800@gmail.com>
2020-10-03 21:43:32 +0200

    Выполнение дз 7

--- js/task-5.js
+++ js/task-5.js
@@ -1,13 +1,14 @@
 // Напиши скрипт который, при наборе текста в инпуте input#name - input(событие input),
 //   подставляет его текущее значение в span#name - output.
 // Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
-const inputBtn = document.querySelector(".js-input-button");
 const inputEl = document.querySelector("#name-input");
 const inputValueEl = inputEl.value;
 const nameEl = document.querySelector("#name-output");
 
-inputBtn.addEventListener("click", () => {
-  if (inputValueEl === !undefined) {
-    nameEl.textContent = inputValueEl;
+inputEl.addEventListener(`input`, (event) => {
+  if (event.currentTarget.value !== "") {
+    nameEl.textContent = event.currentTarget.value;
+  } else if (event.currentTarget.value === "") {
+    nameEl.textContent = `незнакомец`;
   }
 });
--- js/task-8.js
+++ js/task-8.js
@@ -1,101 +1,51 @@
-// filter, includes и map
-// Получи массив имен всех пользователей у которых есть друг с заданным именем.
+// Напиши скрипт создания и очистки коллекции элементов.
+// Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция.
+// При нажатии на кнопку Очистить, коллекция элементов очищается.
 
-// Используй деструктурирующее присваивание для параметра функции { friends } и({ name })
-// без пробелов и переносов на новую строку
+// Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число.
+// Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
 
-// Используй только перебирающие методы массива которые не изменяют(не мутируют) исходный массив.
-//   Т.е.нельзя использовать for, splice, push и т.п.мутирующие методы.
+// Каждый созданный div:
 
-const users = [
-  {
-    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37,
-  },
-  {
-    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34,
-  },
-  {
-    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24,
-  },
-  {
-    id: "249b6175-5c30-44c6-b154-f120923736f5",
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21,
-  },
-  {
-    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27,
-  },
-  {
-    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38,
-  },
-  {
-    id: "e1bf46ab-7168-491e-925e-f01e21394812",
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39,
-  },
-];
+// Имеет случайный rgb цвет фона
+// Размеры самого первого div - 30px на 30px
+// Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
+// Создай функцию destroyBoxes(), которая очищает div#boxes.
 
-const getUsersWithFriend = (array, friendName) =>
-  array
-    .filter(({ friends }) => friends.includes(friendName))
-    .map(({ name }) => name);
-console.log(getUsersWithFriend(users, "Briana Decker"));
+const controlsEl = document.querySelector("#controls");
+const inputEl = document.querySelector("#controls > input");
+const renderBtn = document.querySelector('button[data-action="render"]');
+const destroyBtn = document.querySelector('button[data-action="destroy"]');
+const boxesEl = document.querySelector("#boxes");
+
+renderBtn.addEventListener("click", () => {
+  const createBoxes = (amount) => {
+    const w = 30;
+    const h = 30;
+
+    for (let i = 0; i < amount; i += 1) {
+      const divEl = document.createElement("div");
+      divEl.style.width = `${w + i * 10}px`;
+      divEl.style.height = `${h + i * 10}px`;
+      divEl.style.backgroundColor = randomColor();
+      function randomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${b}, ${g})`;
+      }
+
+      boxesEl.appendChild(divEl);
+    }
+  };
+  createBoxes(Number(inputEl.value));
+});
+
+destroyBtn.addEventListener("click", () => {
+  const destroyBoxes = () => {
+    while (boxesEl.firstChild) {
+      boxesEl.firstChild.remove("div");
+    }
+  };
+  destroyBoxes();
+});
